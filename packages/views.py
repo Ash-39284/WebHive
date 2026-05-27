eba06a31a92399bacbb6d7274e204687de395b73
Ash-39284 <ashroberts98@icloud.com>
2026-05-27 20:52:20 +0100

    Add packages to database

--- packages/views.py
+++ packages/views.py
@@ -4,4 +4,3 @@ from .models import Package
 def packages(request):
     packages = Package.objects.filter(is_active=True)
     return render(request, 'packages/packages.html', {'packages': packages})
-
