3c838c79054694e926b3ba0925502eb323e72f97
Ash-39284 <ashroberts98@icloud.com>
2026-05-27 19:55:23 +0100

    Add admin views for the packages app

--- packages/admin.py
+++ packages/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Package, PackageFeature
 
-# Register your models here.
+class PackageFeatureInline(admin.TabularInline):
+    model = PackageFeature
+    extra = 1
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'price', 'is_recommended', 'is_active', 'display_order']
+    inlines = [PackageFeatureInline]
