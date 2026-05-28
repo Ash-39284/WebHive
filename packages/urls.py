2b1f64c5727b6d2f709b6821b493228a0214d00c
Ash-39284 <ashroberts98@icloud.com>
2026-05-28 14:04:21 +0100

    Add Stripe webhook to urls.py/ settings.py. also added secret_key to .env file

--- packages/urls.py
+++ packages/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('packages/', views.packages, name='packages'),
     path('packages/checkout/<int:package_id>/', views.checkout, name='checkout'),
     path('packages/success/', views.success, name='payment_success'),
+    path('packages/webhook/', views.stripe_webhook, name='stripe_webhook'),
 ]
