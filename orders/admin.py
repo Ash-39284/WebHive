dac2e503bbdec1b46e782cfee1dbc9217a3ffdef
Ash-39284 <ashroberts98@icloud.com>
2026-05-28 14:51:11 +0100

    Add admin models to orders app

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Order, Payment
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'package', 'order_total', 'status', 'created_at']
+    list_filter = ['status']
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order', 'amount', 'currency', 'status', 'paid_at']
