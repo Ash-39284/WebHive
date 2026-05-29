f0de2256a7316038cbe146d9f1786fa458187113
Ash-39284 <ashroberts98@icloud.com>
2026-05-29 08:26:36 +0100

    Add models to contact app

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=255)
+    description = models.TextField()
+    is_active = models.BooleanField(default=True)
+    display_order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['display_order']
+
+    def __str__(self):
+        return self.name
+
+
+class ContactEnquiry(models.Model):
+
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('read', 'Read'),
+        ('responded', 'Responded'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='enquiries')
+    service = models.ForeignKey(Service, on_delete=models.SET_NULL, null=True, blank=True, related_name='enquiries')
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=255)
+    message = models.TextField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new')
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    responded_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-submitted_at']
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
